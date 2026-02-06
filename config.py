a1e56cb670937fee7be24712c383e3cd9a4b62ca
sujay5372bot <sujay53721@gmail.com>
2026-02-06 06:37:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,22 +2,32 @@
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-from os import environ 
-
-class Config:
-    API_ID = int(environ.get("API_ID", ""))
-    API_HASH = environ.get("API_HASH", "")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
-    BOT_OWNER = int(environ.get("BOT_OWNER", ""))
-
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-class temp(object): 
+import os
+from os import environ
+
+class Config:
+    API_ID = int(environ.get("API_ID", "0"))
+    API_HASH = environ.get("API_HASH", "")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "")
+    BOT_SESSION = environ.get("BOT_SESSION", "vjbot")
+    DATABASE_URI = environ.get("DATABASE_URI", "")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
+    BOT_OWNER = int(environ.get("BOT_OWNER", "0"))
+
+    # Login feature
+    LOGIN_SYSTEM = bool(os.environ.get('LOGIN_SYSTEM', True))
+
+    if LOGIN_SYSTEM == False:
+        STRING_SESSION = os.environ.get("STRING_SESSION", "")
+    else:
+        STRING_SESSION = None
+
+
+class temp(object):
     lock = {}
     CANCEL = {}
     forwardings = 0
@@ -27,3 +37,4 @@ class temp(object):
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
